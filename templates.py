e6f1f0befe23559304f5d074efde90dbd04d8cff
pedrodevbr <ordep.vb@gmail.com>
2024-02-18 22:13:09 -0300

    to deploy

---
+++ templates.py
@@ -0,0 +1,11 @@
+# Template Consulta SMIT
+
+
+def consulta_smit(codigo, texto, lmrs, consumo):
+
+    consulta = f"Prezados,
+    O codigo {codigo} - {texto} restou deserto na ultima licitação.
+    Favor indicar uma referencia substituta 
+    segue o historico 
+    "
+    return consulta
